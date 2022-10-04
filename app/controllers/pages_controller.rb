95c7837cae36a952a0cea2335407d0aa51b6bdeb
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-04 02:39:16 -0500

    add contact route/controller/view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def home
-
   end
 
   def about
-    
+  end
+
+  def contact
+    @members = %w[alex avelina andrea andrew anthony amanda boris bob bethany betty billy charly cindy catherine collin celina].sort
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: "pages#home"
   get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end

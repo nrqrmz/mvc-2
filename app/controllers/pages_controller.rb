6abf2f7b82dc50e524de7c42f632904770fee4fa
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-04 02:50:17 -0500

    add search form and logic for team members

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,9 @@ class PagesController < ApplicationController
 
   def contact
     @members = %w[alex avelina andrea andrew anthony amanda boris bob bethany betty billy charly cindy catherine collin celina].sort
+
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
   end
 end
